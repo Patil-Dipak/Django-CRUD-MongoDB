6d31139e6165def90ac0b4b4f19b4f768b3423c6
Patil-Dipak <dips281999@gmail.com>
2021-09-18 18:25:00 +0530

    Added the Dept class in models.py of api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Dept(models.Model):
+    DeptId = models.AutoField(primary_key=True)
+    DeptName = models.CharField(max_length=500)
+
