610157b2ddee121b62d7af50c91c799a5f92f047
Patil-Dipak <dips281999@gmail.com>
2021-09-19 14:41:03 +0530

    Added the Employee class in models.py

--- api/models.py
+++ api/models.py
@@ -6,3 +6,8 @@ class Dept(models.Model):
     DeptId = models.AutoField(primary_key=True)
     DeptName = models.CharField(max_length=500)
 
+class Employee(models.Model):
+    EmployeetId = models.AutoField(primary_key=True)
+    EmployeetName = models.CharField(max_length=500)
+    Dept = models.CharField(max_length=500)
+    DateOfJoining = models.DateField()
