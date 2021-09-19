6a37515a0cf8f1d64321b5f9232fc7d49fe6043a
Patil-Dipak <dips281999@gmail.com>
2021-09-19 15:04:29 +0530

    added the mongo db setting in settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -76,11 +76,19 @@ WSGI_APPLICATION = 'CRUD.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+# Mongo db setting
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'testdb',
     }
 }
 
