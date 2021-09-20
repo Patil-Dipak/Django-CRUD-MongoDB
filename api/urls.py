cb41b708af1ed22ba0549835cb37a63196b94d7c
Patil-Dipak <dips281999@gmail.com>
2021-09-20 23:05:05 +0530

    Added the employee urls in api app urls.py file

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^department$',views.deptApi),
     url(r'^department/([0-9]+)$',views.deptApi),
+    
+    url(r'^employee$',views.employeeApi),
+    url(r'^employee/([0-9]+)$',views.employeeApi),
 ]
