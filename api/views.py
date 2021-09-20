33eea46cf652f4482a48c7f458ef96af16e04da5
Patil-Dipak <dips281999@gmail.com>
2021-09-20 22:00:57 +0530

    Added the deptApi method in views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
+from django.http import request
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
+from .models import Dept, Employee
+from .serializers import DeptSerializer, EmployeeSerializer
 # Create your views here.
+
+@csrf_exempt
+def deptApi(request, id = 0 ):
+    if request.method == "GET":
+        dept = Dept.objects.all()
+        dept_serializers = DeptSerializer(dept, many = True)
+        return JsonResponse(dept_serializers.data, safe = True)
+    
